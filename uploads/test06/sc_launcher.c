26444d9920e17e88ff59879904129bf54621a3ed
Alibi Yermekov <ayermeko@c2r3s2.42prague.com>
2025-06-20 18:50:11 +0200

    dfsdgs

--- uploads/test06/sc_launcher.c
+++ uploads/test06/sc_launcher.c
@@ -1,8 +1,15 @@
 #include "shellcode.h"
-#include <unistd.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
 
 int main()
 {
-    execl("/bin/sh", "sh", 0, 0);
+    void *exec_mem = mmap(NULL, shellcode_len,  PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    if (exec_mem == MAP_FAILED) {
+        return 1;
+    }
+    memcpy(exec_mem, shellcode, shellcode_len);
+    void (*sc)() = (void (*)())exec_mem;
+    sc();
 }
