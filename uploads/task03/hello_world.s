c9470670838dcaabfcc22bb7d745f5b6cafab77f
Alibi Yermekov <ayermeko@c2r3s2.42prague.com>
2025-06-20 17:19:08 +0200

    d

---
+++ uploads/task03/hello_world.s
@@ -0,0 +1,16 @@
+section .data
+    msg db  "Hello World!", 10
+
+section .text
+	global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 13
+    syscall
+
+	mov rdi, 4242
+	mov rax, 60
+	syscall
